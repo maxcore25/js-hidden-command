ef24faf79b8941b7b9597c502b64cedee582f1dc
maxcore25 <mkorobov2002@mail.ru>
2022-02-20 10:18:27 +0300

    Small Naming Refactoring

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-let str = '';
-const reference = 'hello';
-const t1 = '72|105|32|116|104|101|114|101|33|32|73|32|97|109|32|77|97|120|33';
-const t2 =
+let buffer = '';
+const command = 'hello';
+const str1 = '72|105|32|116|104|101|114|101|33|32|73|32|97|109|32|77|97|120|33';
+const str2 =
   '77|121|32|71|105|116|72|117|98|58|32|104|116|116|112|115|58|47|47|103|105|116|104|117|98|46|99|111|109|47|109|97|120|99|111|114|101|50|53';
 
 document.addEventListener('keypress', event => {
-  str += event.key;
-  console.log(str);
-  if (reference.indexOf(str) !== 0) {
-    str = '';
+  buffer += event.key;
+  if (command.indexOf(buffer) !== 0) {
+    buffer = '';
     return;
   }
-  if (str === reference) {
-    console.clear();
-    console.log(text(t1));
-    console.log(text(t2));
-    str = '';
+  if (buffer === command) {
+    console.log(text(str1));
+    console.log(text(str2));
+    buffer = '';
   }
 });
 
